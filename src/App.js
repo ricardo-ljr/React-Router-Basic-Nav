606d8ba9238f354903598134936ddbd723138f35
ricardo-ljr <ricardo.leitejr95@gmail.com>
2020-01-27 15:49:49 -0700

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
+import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" />
+    <Route exact path="/about" />
+    <Route exact path="/contact" />
   </div>
 );
 
